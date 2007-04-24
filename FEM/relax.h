e6ab9e1386ad7323ee5ee1f28e561eee5d33167b
Olaf Kolditz <olaf.kolditz@ufz.de>
2007-04-24 15:27:20 +0000

    removing old files
    git-svn-id: https://svn.ufz.de/svn/ogs/trunk/sources@984 3895a583-e809-0410-9bb6-aa9463026377

--- FEM/relax.h
+++
@@ -1,7 +0,0 @@
-
-
-
-
-extern void RelaxInit(char *name);
-extern void RelaxDestroy(void);
-extern void RelaxIterationChanges(char *name, double *ergebnis, int iterationsschritt, int ndx0, int ndx1, int methode, double *param);
