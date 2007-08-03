25603e129ee84c964f6e21118a3ac2d035282f98
Olaf Kolditz <olaf.kolditz@ufz.de>
2007-08-03 08:18:42 +0000

    new Version
    git-svn-id: https://svn.ufz.de/svn/ogs/trunk/sources@1085 3895a583-e809-0410-9bb6-aa9463026377

--- FEM/makros.h
+++ FEM/makros.h
@@ -22,9 +22,9 @@
   /* Floating-Point */
 
 /* ROCKFLOW-Version */
-#define ROCKFLOW_VERSION    "4.5.06"
-#define RF_SHELL_VERSION    "4.5.04PCH"
-#define ROCKFLOW_DATE       "19.07.2007"
+#define ROCKFLOW_VERSION    "4.5.07OK"
+#define RF_SHELL_VERSION    "4.5.07OK"
+#define ROCKFLOW_DATE       "02.08.2007"
 
 /* Definitionen von Makros zur Steuerung der bedingten Compilierung */
 #define SWITCHES
