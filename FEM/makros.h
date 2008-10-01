8650354edbfd31fb661b48f0fac2d3fcc31aeb7b
Olaf Kolditz <olaf.kolditz@ufz.de>
2008-10-01 14:23:24 +0000

    OK: - Updating version number
    git-svn-id: https://svn.ufz.de/svn/ogs/trunk/sources@1927 3895a583-e809-0410-9bb6-aa9463026377

--- FEM/makros.h
+++ FEM/makros.h
@@ -22,9 +22,9 @@
   /* Floating-Point */
 
 /* ROCKFLOW-Version */
-#define ROCKFLOW_VERSION    "4.7.07 (HS)"
-#define RF_SHELL_VERSION    "4.7.06 (NB)"
-#define ROCKFLOW_DATE       "23.09.2008"
+#define ROCKFLOW_VERSION    "4.7.08 (MR)"
+#define RF_SHELL_VERSION    "4.7.08 (MR)"
+#define ROCKFLOW_DATE       "01.10.2008"
 
 /* Definitionen von Makros zur Steuerung der bedingten Compilierung */
 #define SWITCHES
