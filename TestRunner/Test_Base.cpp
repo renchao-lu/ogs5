311c78ce628d032f12deaf00ba61c18cab000ddd
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2010-07-03 11:39:41 +0000

    needed to change also test codes
    git-svn-id: https://svn.ufz.de/svn/ogs/trunk/sources@5692 3895a583-e809-0410-9bb6-aa9463026377

--- TestRunner/Test_Base.cpp
+++ TestRunner/Test_Base.cpp
@@ -13,7 +13,7 @@
 TEST(Base, SwapInt) {
 	int arg0 = 5;
 	int arg1 = 10;
-	swap(arg0, arg1);	
+    BASELIB::swap(arg0, arg1);	
 	ASSERT_EQ ( arg0, 10 );
 	ASSERT_EQ ( arg1, 5 );
 }
@@ -21,7 +21,7 @@ TEST(Base, SwapInt) {
 TEST(Base, SwapDouble) {
 	double arg0 = 5.0;
 	double arg1 = 10.0;
-	swap(arg0, arg1);	
+	BASELIB::swap(arg0, arg1);	
 	ASSERT_EQ ( arg0, 10.0 );
 	ASSERT_EQ ( arg1, 5.0 );
 }
@@ -29,7 +29,7 @@ TEST(Base, SwapDouble) {
 TEST(Base, SwapString) {
 	std::string arg0 = "5";
 	std::string arg1 = "10";
-	swap(arg0, arg1);	
+	BASELIB::swap(arg0, arg1);	
 	ASSERT_EQ ( arg0, std::string("10") );
 	ASSERT_EQ ( arg1, std::string("5") );
 }
