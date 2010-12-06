c83eb49aaab88af3f856b65a29cf2aae5c47a105
Lars Bilke <lars.bilke@ufz.de>
2010-12-06 12:53:09 +0000

    git-svn-id: https://svn.ufz.de/svn/ogs/trunk/sources@7062 3895a583-e809-0410-9bb6-aa9463026377

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -14,35 +14,41 @@ set +e >/dev/null
 
 # Run FEM benchmarks
 cd build_fem
-if [$1 = 'ex'] then
-	ctest -R EXCEED -E 'JOD|AllTests|FILE|thm_decov|h_us_line|h_us_quad|h_us_tri|w_exp' -j 32 > ../benchOut.txt
-	ctest -R 'EXCEED|FILECOMPARE' -E 'JOD|thm_decov|h_us_line|h_us_quad|h_us_tri|w_exp' >> ../benchOut.txt
+if  [ "$1" = "ex" ]
+then
+	ctest -R 'EXCEED' -E 'JOD|AllTests|FILE' -j 32 >> ../benchOut.txt
+	ctest -R 'EXCEEDING_FILECOMPARE' -E 'JOD' >> ../benchOut.txt
 	cd .. >/dev/null
+	
 	cd build_brns
 	ctest -R 'EXCEED' -E 'FILE' -j 32 >> ../benchOut.txt
-	ctest -R 'EXCEED|FILECOMPARE' >> ../benchOut.txt
+	ctest -R 'EXCEEDING_FILECOMPARE' >> ../benchOut.txt
 	cd .. >/dev/null
+	
+	cd build_pqc
+	ctest -R 'EXCEED' -E 'FILE' -j 32 >> ../benchOut.txt
+	ctest -R 'EXCEEDING_FILECOMPARE' >> ../benchOut.txt
+	
 else
-	ctest -E 'JOD|AllTests|FILE|EXCEED|thm_decov|h_us_line|h_us_quad|h_us_tri|w_exp' -j 32 > ../benchOut.txt
-	ctest -R FILECOMPARE -E 'JOD|EXCEED|thm_decov|h_us_line|h_us_quad|h_us_tri|w_exp' >> ../benchOut.txt
+	ctest -E 'JOD|AllTests|FILE|EXCEED' -j 32 > ../benchOut.txt
+	ctest -R 'FILECOMPARE' -E 'JOD|EXCEED' >> ../benchOut.txt
 	cd .. >/dev/null
+	
 	cd build_brns
-	ctest -E "FILE|EXCEED" -j 32 >> ../benchOut.txt
-	ctest -R FILECOMPARE -E "EXCEED" >> ../benchOut.txt
+	ctest -E 'FILE|EXCEED' -j 32 >> ../benchOut.txt
+	ctest -R 'FILECOMPARE' -E 'EXCEED' >> ../benchOut.txt
 	cd .. >/dev/null
+
+	cd build_pqc
+	ctest -E 'FILE|EXCEED' -j 32 >> ../benchOut.txt
+	ctest -R 'FILECOMPARE' -E 'EXCEED' >> ../benchOut.txt
 fi
-
-# Run FEM_PQC benchmarks
-#cd build_pqc
-#ctest -E "FILE|EXCEED" -j 32 >> ../benchOut.txt
-#ctest -R FILECOMPARE -E "EXCEED" >> ../benchOut.txt
-#cd .. >/dev/null
-
+cd .. >/dev/null
 
 # Print results
 cat benchOut.txt
 
 # Send emails on errors
-python26 ~/bin/sendmail/sendmail.py
+#python26 ~/bin/sendmail/sendmail.py
 
 set -e >/dev/null
