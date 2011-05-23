41cdf08b938dd38f89acd3cf7c08575fc87dc933
singha <singha@3895a583-e809-0410-9bb6-aa9463026377>
2011-05-23 12:20:31 +0000

    fix
    git-svn-id: https://svn.ufz.de/svn/ogs/trunk/sources@8370 3895a583-e809-0410-9bb6-aa9463026377

--- FEM/fem_ele_std.cpp
+++ FEM/fem_ele_std.cpp
@@ -626,12 +626,22 @@ namespace FiniteElement
             }
             break;
          case 'M':                                // Mass transport
-            if(T_Flag)
-			{
-               cpl_pcs = PCSGet("HEAT_TRANSPORT");
-               idx_c0 = cpl_pcs->GetNodeValueIndex("TEMPERATURE1");
-               idx_c1 = idx_c0+1;
-		 }
+		if(C_Flag && T_Flag)
+	{
+	if(cpl_pcs == NULL)
+	{
+	cpl_pcs = PCSGet("PTC_FLOW");
+	if(cpl_pcs)
+	idx_c0 = cpl_pcs->GetNodeValueIndex("PRESSURE1");
+	idx_c1 = idx_c0+1;
+	}
+	}                              // Mass transport
+	else if(T_Flag)
+	{
+	cpl_pcs = PCSGet("HEAT_TRANSPORT");
+	idx_c0 = cpl_pcs->GetNodeValueIndex("TEMPERATURE1");
+	idx_c1 = idx_c0+1;
+	} 
             break;
          case 'O':                                // Liquid flow
             break;
