0a41eb9cdbcb58a30cc0d2ba5d2090c03eef286b
Lars Bilke <lars.bilke@ufz.de>
2011-08-24 09:29:39 +0000

    Simplifying.
    git-svn-id: https://svn.ufz.de/svn/ogs/trunk/sources@9200 3895a583-e809-0410-9bb6-aa9463026377

--- scripts/build_fem.bat
+++ scripts/build_fem.bat
@@ -1,20 +1,19 @@
 :: Setup Visual Studio environment
-call "%VS80COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
-call "%VS90COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
+call setup_vs.bat %1
 
 :: Goto sources directory
 cd ..
 
 :: Cleanup
-rd /S /Q build_fem
+rd /S /Q Release build_fem
 
-:: Build
+:: Build FEM
 mkdir build_fem
 cd build_fem
-cmake -DOGS_FEM=ON -DOGS_PACKAGING=ON -DOGS_PACKAGING_ZIP=ON ..
+cmake -G %generator% -DOGS_FEM=ON -DOGS_DONT_USE_QT=ON -DOGS_PACKAGING=ON -DOGS_PACKAGING_ZIP=ON ..
 cmake ..
 devenv OGS-FEM-5.sln /Build Release
-
-:: Package
 cmake ..
 devenv OGS-FEM-5.sln /Build Release /Project PACKAGE
+cd ..
+cd scripts
