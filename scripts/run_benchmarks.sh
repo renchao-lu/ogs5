7118e180ee5c90d74772c3acebcb8975da9b8b60
Lars Bilke <lars.bilke@ufz.de>
2012-06-15 14:35:22 +0000

    Failed exceeding becnhmarks should be shown in Jenkins now.
    git-svn-id: https://svn.ufz.de/svn/ogs/trunk/sources@10967 3895a583-e809-0410-9bb6-aa9463026377

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -41,27 +41,27 @@ else
 	exit 1
 fi
 
-# Don´t abort on errors
-set +e >/dev/null
-
 # Run FEM benchmarks
 cd $BUILD_LOCATION/build_fem
 if  [ $RUN_EXCEEDING ]; then
-	ctest -R 'EXCEED' -E 'JOD|Tests|FILE' -j $NUM_PROCESSORS > ../benchOut.txt
-	ctest -R 'EXCEEDING_FILECOMPARE' -E 'JOD' >> ../benchOut.txt
+	ctest -R 'EXCEED' -E 'JOD|Tests|FILE' -j $NUM_PROCESSORS
+	ctest -R 'EXCEEDING_FILECOMPARE' -E 'JOD'
 	
 	cd $BUILD_LOCATION/build_brns
-	ctest -R 'EXCEED' -E 'Tests|FILE' -j $NUM_PROCESSORS >> ../benchOut.txt
-	ctest -R 'EXCEEDING_FILECOMPARE' >> ../benchOut.txt
+	ctest -R 'EXCEED' -E 'Tests|FILE' -j $NUM_PROCESSORS
+	ctest -R 'EXCEEDING_FILECOMPARE'
 	
 	cd $BUILD_LOCATION/build_pqc
-	ctest -R 'EXCEED' -E 'Tests|FILE' -j $NUM_PROCESSORS >> ../benchOut.txt
-	ctest -R 'EXCEEDING_FILECOMPARE' >> ../benchOut.txt
+	ctest -R 'EXCEED' -E 'Tests|FILE' -j $NUM_PROCESSORS
+	ctest -R 'EXCEEDING_FILECOMPARE'
 	
 	cd $BUILD_LOCATION/build_gems
-	ctest -R 'EXCEED' -E 'Tests|FILE' -j $NUM_PROCESSORS >> ../benchOut.txt
-	ctest -R 'EXCEEDING_FILECOMPARE' >> ../benchOut.txt
+	ctest -R 'EXCEED' -E 'Tests|FILE' -j $NUM_PROCESSORS
+	ctest -R 'EXCEEDING_FILECOMPARE'
 else
+	# Don´t abort on errors
+	set +e >/dev/null
+
 	ctest -E 'Tests|FILE|EXCEED' -j $NUM_PROCESSORS > ../benchOut.txt
 	ctest -R 'FILECOMPARE' -E 'EXCEED' >> ../benchOut.txt
 	
@@ -76,20 +76,20 @@ else
 	cd $BUILD_LOCATION/build_gems
 	ctest -E 'FILE|EXCEED|Tests' -j $NUM_PROCESSORS >> ../benchOut.txt
 	ctest -R 'FILECOMPARE' -E 'EXCEED' >> ../benchOut.txt
+
+	# Print results
+	cat benchOut.txt
+	
+	cd $SOURCE_LOCATION/scripts
+	# Send emails on errors
+	FILESIZE=$(stat -c %s $BUILD_LOCATION/svnInfo.txt)
+	if [ "$FILESIZE" > "0" ] ; then
+	  echo "Running process_benchmark_job.rb"
+	  cd process_benchmark_job
+	  ruby process_benchmark_job.rb $BUILD_LOCATION/svnInfo.txt $BUILD_LOCATION/benchOut.txt $HUDSON_EMAIL $1
+	  cd ..
+	fi
+
+	set -e >/dev/null
 fi
 cd $BUILD_LOCATION >/dev/null
-
-# Print results
-cat benchOut.txt
-
-cd $SOURCE_LOCATION/scripts
-# Send emails on errors
-FILESIZE=$(stat -c %s $BUILD_LOCATION/svnInfo.txt)
-if [ "$FILESIZE" > "0" ] ; then
-  echo "Running process_benchmark_job.rb"
-  cd process_benchmark_job
-  ruby process_benchmark_job.rb $BUILD_LOCATION/svnInfo.txt $BUILD_LOCATION/benchOut.txt $HUDSON_EMAIL $1
-  cd ..
-fi
-
-set -e >/dev/null
