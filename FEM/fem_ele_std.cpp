2c796c3e008b1b6ce2d6a0679bb8a526af8baeb1
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2012-11-19 13:52:50 +0000

    use reference gas density instead of current density if density model=2
    git-svn-id: https://svn.ufz.de/svn/ogs/trunk/sources@11500 3895a583-e809-0410-9bb6-aa9463026377

--- FEM/fem_ele_std.cpp
+++ FEM/fem_ele_std.cpp
@@ -1642,7 +1642,7 @@ double CFiniteElementStd::CalCoefMass2(int dof_index)
 			dens_arg[1] = TG;
 		/// d dens_g/dp_g:
 		if (GasProp->density_model==2)//dens_g/dp_g = drho_dp 02.2012. WX
-			val = (1.0 - Sw) * poro * rho_ga * GasProp->drho_dp / rhow;
+			val = (1.0 - Sw) * poro * GasProp->rho_0 * GasProp->drho_dp / rhow;
 		else
 			val = (1.0 - Sw) * poro * (GasProp->Density(dens_arg) - rho_ga) / (pert * rhow);
 		break;
