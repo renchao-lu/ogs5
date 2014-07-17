a4b0484f39d0601ef886001b40bf38c4da8b77a5
Wenqing Wang <wenqing.wang@ufz.de>
2014-07-17 17:32:16 +0200

    Record cpu time for assembly

--- FEM/rf_pcs.cpp
+++ FEM/rf_pcs.cpp
@@ -74,8 +74,6 @@
 /*-----------------------------------------------------------------------*/
 /* Tools */
 
-
-
 #ifdef MFC                                        //WW
 #include "rf_fluid_momentum.h"
 #endif
@@ -393,6 +391,11 @@ Problem* CRFProcess::getProblemObjectPointer () const
 **************************************************************************/
 CRFProcess::~CRFProcess(void)
 {
+#ifdef USE_PETSC
+       if(myrank == 0)
+           PetscPrintf(PETSC_COMM_WORLD,"\t\n>>Total Wall clock time in assembling (with PETSC):%f s\n",cpu_time_assembly);
+  
+#endif
 	long i;
 	//----------------------------------------------------------------------
 	// Finite element
@@ -5368,6 +5371,15 @@ void CRFProcess::AddFCT_CorrectionVector()
  **************************************************************************/
 void CRFProcess::GlobalAssembly()
 {
+#ifdef USE_PETSC
+      PetscLogDouble v1,v2;
+#ifdef USEPETSC34
+       PetscTime(&v1);
+#else
+       PetscGetTime(&v1);
+#endif
+#endif
+
 	// Tests
 	if (!Tim)
 		Tim = TIMGet(convertProcessTypeToString(this->getProcessType()));
@@ -5550,6 +5562,15 @@ void CRFProcess::GlobalAssembly()
 		//eqs_new->AssembleMatrixPETSc(MAT_FINAL_ASSEMBLY );
 #endif
 	}
+
+#ifdef USE_PETSC
+#ifdef USEPETSC34
+       PetscTime(&v2);
+#else
+       PetscGetTime(&v2);
+#endif
+       cpu_time_assembly += v2 - v1;
+#endif
 }
 
 //--------------------------------------------------------------------
