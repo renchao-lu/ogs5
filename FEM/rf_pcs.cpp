c215544a59c3b17522da6502372d63efa826c403
Thomas Fischer <thomas.fischer@ufz.de>
2014-08-17 21:42:09 +0200

    Rename attributes/methods of class BoundaryCondition for better readability.

--- FEM/rf_pcs.cpp
+++ FEM/rf_pcs.cpp
@@ -6541,7 +6541,7 @@ void CRFProcess::DDCAssembleGlobalMatrix()
 				{
 
 					//MW calculate pressure from given head at bc with density, gravity constant and geodetic height for PRESSURE as primary variable
-					if ( m_bc->PressureAsHead() == 0 )
+					if ( m_bc->getPressureAsHeadModel() == 0 )
 					{
 						//use current density at node
 						const double local_density = MFPGetNodeValue(m_bc_node->msh_node_number, "DENSITY", 0);
@@ -6550,7 +6550,7 @@ void CRFProcess::DDCAssembleGlobalMatrix()
 						// pressure = gravitational_constant * density * ( head - geodetic_height )
 						bc_value = fac * gravity_constant * local_density * ( time_fac * m_bc_node->node_value - local_node_elevation );
 					}
-					else if (m_bc->PressureAsHead() == 1)
+					else if (m_bc->getPressureAsHeadModel() == 1)
 					{
 						//use given density
 						const double local_density = m_bc->getPressureAsHeadDensity();
@@ -7312,7 +7312,7 @@ bool CRFProcess::checkConstrainedBC(CBoundaryCondition const & bc, CBoundaryCond
 			CRFProcess* pcs(PCSGet(local_constrained.constrainedProcessType));
 
 			//other variable
-			for (std::size_t k = 0; k < m_pcs->GetPrimaryVNumber(); k++)
+			for (std::size_t k = 0; k < pcs->GetPrimaryVNumber(); k++)
 			{
 				if (pcs->GetPrimaryVName(k) == FiniteElement::convertPrimaryVariableToString(local_constrained.constrainedPrimVar))
 				{
