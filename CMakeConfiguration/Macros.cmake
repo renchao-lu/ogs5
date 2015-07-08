d2d4542a5fabef8f39628757527afbe2457e9aee
Christoph Lehmann <christoph.lehmann@ufz.de>
2015-07-08 06:45:18 +0000

    Changed to lower-case function naming in CMake.
    See https://github.com/ufz/ogs/pull/597
    
    git-svn-id: https://svn.ufz.de/svn/ogs/trunk/sources@14280 3895a583-e809-0410-9bb6-aa9463026377

--- CMakeConfiguration/Macros.cmake
+++ CMakeConfiguration/Macros.cmake
@@ -32,38 +32,38 @@ endif()
 
 # Convert environment variables
 if(NOT $ENV{LIBRARIES_DIR} STREQUAL "")
-	STRING(REGEX REPLACE "\\\\" "/" LIBRARIES_DIR $ENV{LIBRARIES_DIR})
+	string(REGEX REPLACE "\\\\" "/" LIBRARIES_DIR $ENV{LIBRARIES_DIR})
 endif()
 
-MACRO(COPY_FILE_IF_CHANGED in_file out_file target)
-	IF(${in_file} IS_NEWER_THAN ${out_file})
+macro(COPY_FILE_IF_CHANGED in_file out_file target)
+	if(${in_file} IS_NEWER_THAN ${out_file})
 		#message("Copying file: ${in_file} to: ${out_file}")
-		ADD_CUSTOM_COMMAND (
+		add_custom_command (
 				TARGET     ${target}
 				POST_BUILD
 				COMMAND    ${CMAKE_COMMAND}
 				ARGS       -E copy ${in_file} ${out_file}
 		)
-	ENDIF()
-ENDMACRO(COPY_FILE_IF_CHANGED)
+	endif()
+endmacro(COPY_FILE_IF_CHANGED)
 
-MACRO(COPY_FILE_INTO_DIRECTORY_IF_CHANGED in_file out_dir target)
-		GET_FILENAME_COMPONENT(file_name ${in_file} NAME)
+macro(COPY_FILE_INTO_DIRECTORY_IF_CHANGED in_file out_dir target)
+		get_filename_component(file_name ${in_file} NAME)
 		COPY_FILE_IF_CHANGED(${in_file} ${out_dir}/${file_name}
 ${target})
-ENDMACRO(COPY_FILE_INTO_DIRECTORY_IF_CHANGED)
+endmacro(COPY_FILE_INTO_DIRECTORY_IF_CHANGED)
 
 #Copies all the files from in_file_list into the out_dir.
 # sub-trees are ignored (files are stored in same out_dir)
-MACRO(COPY_FILES_INTO_DIRECTORY_IF_CHANGED in_file_list out_dir target)
-	FOREACH(in_file ${in_file_list})
+macro(COPY_FILES_INTO_DIRECTORY_IF_CHANGED in_file_list out_dir target)
+	foreach(in_file ${in_file_list})
 				COPY_FILE_INTO_DIRECTORY_IF_CHANGED(${in_file}
 ${out_dir} ${target})
-		ENDFOREACH(in_file)
-ENDMACRO(COPY_FILES_INTO_DIRECTORY_IF_CHANGED)
+		endforeach(in_file)
+endmacro(COPY_FILES_INTO_DIRECTORY_IF_CHANGED)
 
-MACRO(COPY_FILE_INTO_EXECUTABLE_DIRECTORY in_file target)
-	IF(WIN32)
+macro(COPY_FILE_INTO_EXECUTABLE_DIRECTORY in_file target)
+	if(WIN32)
 		COPY_FILE_INTO_DIRECTORY_IF_CHANGED(
 			${CMAKE_CURRENT_SOURCE_DIR}/${in_file}
 			${EXECUTABLE_OUTPUT_PATH}/Debug
@@ -74,14 +74,14 @@ MACRO(COPY_FILE_INTO_EXECUTABLE_DIRECTORY in_file target)
 			${EXECUTABLE_OUTPUT_PATH}/Release
 			${target}
 		)
-	ELSE(WIN32)
+	else(WIN32)
 		COPY_FILE_INTO_DIRECTORY_IF_CHANGED(
 			${CMAKE_CURRENT_SOURCE_DIR}/${in_file}
 			${EXECUTABLE_OUTPUT_PATH}
 			${target}
 		)
-	ENDIF(WIN32)
-ENDMACRO(COPY_FILE_INTO_EXECUTABLE_DIRECTORY)
+	endif(WIN32)
+endmacro(COPY_FILE_INTO_EXECUTABLE_DIRECTORY)
 
 # Adds a benchmark run.
 # authorName Your short name
@@ -89,89 +89,89 @@ ENDMACRO(COPY_FILE_INTO_EXECUTABLE_DIRECTORY)
 # ogsConfiguration E.g. "OGS_FEM"
 # numProcesses Number of processes for mpirun, Please set to 1 for non-MPI benchmarks
 # Additional arguments add output files to compare
-FUNCTION (ADD_BENCHMARK authorName benchmarkName ogsConfiguration numProcesses)
+function (ADD_BENCHMARK authorName benchmarkName ogsConfiguration numProcesses)
 
-  SET(CONFIG_MATCH FALSE)
-  IF("${ogsConfiguration}" STREQUAL "OGS_FEM" AND OGS_FEM )
-	SET(CONFIG_MATCH TRUE)
-  ENDIF()
+  set(CONFIG_MATCH FALSE)
+  if("${ogsConfiguration}" STREQUAL "OGS_FEM" AND OGS_FEM )
+	set(CONFIG_MATCH TRUE)
+  endif()
 
-  IF("${ogsConfiguration}" STREQUAL "OGS_FEM_SP" AND OGS_FEM_SP)
-	SET(CONFIG_MATCH TRUE)
-  ENDIF()
+  if("${ogsConfiguration}" STREQUAL "OGS_FEM_SP" AND OGS_FEM_SP)
+	set(CONFIG_MATCH TRUE)
+  endif()
 
-  IF("${ogsConfiguration}" STREQUAL "OGS_FEM_GEMS" AND OGS_FEM_GEMS)
-	SET(CONFIG_MATCH TRUE)
-  ENDIF()
+  if("${ogsConfiguration}" STREQUAL "OGS_FEM_GEMS" AND OGS_FEM_GEMS)
+	set(CONFIG_MATCH TRUE)
+  endif()
 
-  IF("${ogsConfiguration}" STREQUAL "OGS_FEM_BRNS" AND OGS_FEM_BRNS)
-	SET(CONFIG_MATCH TRUE)
-  ENDIF()
+  if("${ogsConfiguration}" STREQUAL "OGS_FEM_BRNS" AND OGS_FEM_BRNS)
+	set(CONFIG_MATCH TRUE)
+  endif()
 
-  IF("${ogsConfiguration}" STREQUAL "OGS_FEM_PQC" AND OGS_FEM_PQC)
-	SET(CONFIG_MATCH TRUE)
-  ENDIF()
+  if("${ogsConfiguration}" STREQUAL "OGS_FEM_PQC" AND OGS_FEM_PQC)
+	set(CONFIG_MATCH TRUE)
+  endif()
 
-  IF("${ogsConfiguration}" STREQUAL "OGS_FEM_IPQC" AND OGS_FEM_IPQC)
-	SET(CONFIG_MATCH TRUE)
-  ENDIF()
+  if("${ogsConfiguration}" STREQUAL "OGS_FEM_IPQC" AND OGS_FEM_IPQC)
+	set(CONFIG_MATCH TRUE)
+  endif()
 
-  IF("${ogsConfiguration}" STREQUAL "OGS_FEM_CAP" AND OGS_FEM_CAP)
-    SET(CONFIG_MATCH TRUE)
-  ENDIF()
+  if("${ogsConfiguration}" STREQUAL "OGS_FEM_CAP" AND OGS_FEM_CAP)
+    set(CONFIG_MATCH TRUE)
+  endif()
 
-  IF (UNIX) # Only supported on Linux
-	IF("${ogsConfiguration}" STREQUAL "OGS_FEM_LIS" AND OGS_FEM_LIS)
-	  SET(CONFIG_MATCH TRUE)
-	ENDIF()
-	IF("${ogsConfiguration}" STREQUAL "OGS_FEM_MKL" AND OGS_FEM_MKL)
-	  SET(CONFIG_MATCH TRUE)
-	ENDIF()
-	IF("${ogsConfiguration}" STREQUAL "OGS_FEM_MPI" AND OGS_FEM_MPI)
-	  SET(CONFIG_MATCH TRUE)
-	ENDIF()
-	IF("${ogsConfiguration}" STREQUAL "OGS_FEM_PETSC" AND OGS_FEM_PETSC)
-	  SET(CONFIG_MATCH TRUE)
-	ENDIF()
-	IF("${ogsConfiguration}" STREQUAL "OGS_FEM_PETSC_GEMS" AND OGS_FEM_PETSC_GEMS)
-	  SET(CONFIG_MATCH TRUE)
-	ENDIF()
-  ENDIF (UNIX)
+  if (UNIX) # Only supported on Linux
+	if("${ogsConfiguration}" STREQUAL "OGS_FEM_LIS" AND OGS_FEM_LIS)
+	  set(CONFIG_MATCH TRUE)
+	endif()
+	if("${ogsConfiguration}" STREQUAL "OGS_FEM_MKL" AND OGS_FEM_MKL)
+	  set(CONFIG_MATCH TRUE)
+	endif()
+	if("${ogsConfiguration}" STREQUAL "OGS_FEM_MPI" AND OGS_FEM_MPI)
+	  set(CONFIG_MATCH TRUE)
+	endif()
+	if("${ogsConfiguration}" STREQUAL "OGS_FEM_PETSC" AND OGS_FEM_PETSC)
+	  set(CONFIG_MATCH TRUE)
+	endif()
+	if("${ogsConfiguration}" STREQUAL "OGS_FEM_PETSC_GEMS" AND OGS_FEM_PETSC_GEMS)
+	  set(CONFIG_MATCH TRUE)
+	endif()
+  endif (UNIX)
 
-  IF (CONFIG_MATCH)
-	IF(WIN32)
-	  SET(ogsExe ${EXECUTABLE_OUTPUT_PATH}/Release/ogs)
-	ELSE(WIN32)
-	  SET(ogsExe ${EXECUTABLE_OUTPUT_PATH}/ogs)
-	ENDIF(WIN32)
+  if (CONFIG_MATCH)
+	if(WIN32)
+	  set(ogsExe ${EXECUTABLE_OUTPUT_PATH}/Release/ogs)
+	else(WIN32)
+	  set(ogsExe ${EXECUTABLE_OUTPUT_PATH}/ogs)
+	endif(WIN32)
 
 	# Set timeout
-	STRING(REGEX MATCH "EXCEEDING" benchmarkExceeding ${benchmarkName})
-	IF(benchmarkExceeding)
-		SET(THIS_BENCHMARK_TIMEOUT ${EXCEEDING_BENCHMARK_TIMEOUT})
-	ELSE()
-		SET(THIS_BENCHMARK_TIMEOUT ${BENCHMARK_TIMEOUT})
-	ENDIF()
+	string(REGEX MATCH "EXCEEDING" benchmarkExceeding ${benchmarkName})
+	if(benchmarkExceeding)
+		set(THIS_BENCHMARK_TIMEOUT ${EXCEEDING_BENCHMARK_TIMEOUT})
+	else()
+		set(THIS_BENCHMARK_TIMEOUT ${BENCHMARK_TIMEOUT})
+	endif()
 
-	STRING (REGEX MATCH "[^/]+$" benchmarkStrippedName ${benchmarkName})
-	STRING (LENGTH ${benchmarkName} benchmarkNameLength)
-	STRING (LENGTH ${benchmarkStrippedName} benchmarkStrippedNameLength)
-	MATH (EXPR substringLength ${benchmarkNameLength}-${benchmarkStrippedNameLength})
-	STRING (SUBSTRING ${benchmarkName} 0 ${substringLength} benchmarkDir)
-	STRING (REPLACE "/" "_" benchmarkNameUnderscore ${benchmarkName})
-	STRING (REPLACE "_LONG_" "_" benchmarkNameUnderscore ${benchmarkNameUnderscore})
-	STRING (REPLACE "_EXCEEDING_" "_" benchmarkNameUnderscore ${benchmarkNameUnderscore})
+	string (REGEX MATCH "[^/]+$" benchmarkStrippedName ${benchmarkName})
+	string (LENGTH ${benchmarkName} benchmarkNameLength)
+	string (LENGTH ${benchmarkStrippedName} benchmarkStrippedNameLength)
+	math (EXPR substringLength ${benchmarkNameLength}-${benchmarkStrippedNameLength})
+	string (SUBSTRING ${benchmarkName} 0 ${substringLength} benchmarkDir)
+	string (REPLACE "/" "_" benchmarkNameUnderscore ${benchmarkName})
+	string (REPLACE "_LONG_" "_" benchmarkNameUnderscore ${benchmarkNameUnderscore})
+	string (REPLACE "_EXCEEDING_" "_" benchmarkNameUnderscore ${benchmarkNameUnderscore})
 
 	# Delete output files on testing
-	FOREACH (entry ${ARGN})
-	SET (FILES_TO_DELETE "${FILES_TO_DELETE} ${entry}")
-	ENDFOREACH (entry ${ARGN})
-	IF(OGS_PROFILE)
-		SET(FILES_TO_DELETE "${FILES_TO_DELETE} gmon.out")
-	ENDIF()
+	foreach (entry ${ARGN})
+	set (FILES_TO_DELETE "${FILES_TO_DELETE} ${entry}")
+	endforeach (entry ${ARGN})
+	if(OGS_PROFILE)
+		set(FILES_TO_DELETE "${FILES_TO_DELETE} gmon.out")
+	endif()
 
 	# Adds a benchmark run. This calls AddTest.cmake to execute several steps.
-	ADD_TEST(
+	add_test(
 		${authorName}_BENCHMARK_${benchmarkName}
 		${CMAKE_COMMAND}
 		-DPROJECT_SOURCE_DIR=${PROJECT_SOURCE_DIR}
@@ -190,12 +190,12 @@ FUNCTION (ADD_BENCHMARK authorName benchmarkName ogsConfiguration numProcesses)
 	)
 
 	# compare file differences with python script
-	IF(PYTHONINTERP_FOUND)
-		FILE (REMOVE ${PROJECT_SOURCE_DIR}/../benchmarks/results/temp/temp_${benchmarkNameUnderscore}.txt)
-		FOREACH (entry ${ARGN})
-			FILE (APPEND ${PROJECT_SOURCE_DIR}/../benchmarks/results/temp/temp_${benchmarkNameUnderscore}.txt "${entry}\n")
-		ENDFOREACH (entry ${ARGN})
-		ADD_TEST(
+	if(PYTHONINTERP_FOUND)
+		file (REMOVE ${PROJECT_SOURCE_DIR}/../benchmarks/results/temp/temp_${benchmarkNameUnderscore}.txt)
+		foreach (entry ${ARGN})
+			file (APPEND ${PROJECT_SOURCE_DIR}/../benchmarks/results/temp/temp_${benchmarkNameUnderscore}.txt "${entry}\n")
+		endforeach (entry ${ARGN})
+		add_test(
 			${authorName}_FILECOMPARE_${benchmarkName}
 			${CMAKE_COMMAND} -E chdir ${PROJECT_SOURCE_DIR}/../benchmarks/results
 			${PYTHON_EXECUTABLE}
@@ -205,23 +205,23 @@ FUNCTION (ADD_BENCHMARK authorName benchmarkName ogsConfiguration numProcesses)
 			${authorName}_${benchmarkNameUnderscore}.html
 			../
 		)
-	ENDIF(PYTHONINTERP_FOUND)
+	endif(PYTHONINTERP_FOUND)
 
   # copy benchmark output files to reference directory
-  IF(COPY_BENCHMARKS_TO_REF)
-	FOREACH (entry ${ARGN})
-	  CONFIGURE_FILE( ${PROJECT_SOURCE_DIR}/../benchmarks/${entry} ${PROJECT_SOURCE_DIR}/../benchmarks_ref/${entry} COPYONLY)
-	ENDFOREACH (entry ${ARGN})
-  ENDIF(COPY_BENCHMARKS_TO_REF)
+  if(COPY_BENCHMARKS_TO_REF)
+	foreach (entry ${ARGN})
+	  configure_file( ${PROJECT_SOURCE_DIR}/../benchmarks/${entry} ${PROJECT_SOURCE_DIR}/../benchmarks_ref/${entry} COPYONLY)
+	endforeach (entry ${ARGN})
+  endif(COPY_BENCHMARKS_TO_REF)
 
-  ENDIF(CONFIG_MATCH)
+  endif(CONFIG_MATCH)
 
-ENDFUNCTION (ADD_BENCHMARK authorName benchmarkName ogsConfiguration filesToCompare numProcesses)
+endfunction (ADD_BENCHMARK authorName benchmarkName ogsConfiguration filesToCompare numProcesses)
 
 # Checks if a valid ogs configuration is given
-FUNCTION(CHECK_CONFIG)
+function(CHECK_CONFIG)
 
-	SET(configs
+	set(configs
 		"${OGS_FEM}"
 		"${OGS_FEM_SP}"
 		"${OGS_FEM_MPI}"
@@ -235,22 +235,22 @@ FUNCTION(CHECK_CONFIG)
 		"${OGS_FEM_PETSC}"
 		"${OGS_FEM_PETSC_GEMS}"
         "${OGS_FEM_CAP}")
-	SET(counter 0)
+	set(counter 0)
 
-	FOREACH(config ${configs})
-		IF(config)
-			MATH(EXPR counter "${counter} + 1")
-		ENDIF(config)
-#		MESSAGE(STATUS "config test ${config} found total ${counter}")
-	ENDFOREACH()
-	IF(counter EQUAL 0)
-		MESSAGE(STATUS "No configuration specified. Assuming default configuration...")
-		SET(OGS_FEM ON)
-                SET(counter 1)
-	ENDIF(counter EQUAL 0)
+	foreach(config ${configs})
+		if(config)
+			math(EXPR counter "${counter} + 1")
+		endif(config)
+#		message(STATUS "config test ${config} found total ${counter}")
+	endforeach()
+	if(counter EQUAL 0)
+		message(STATUS "No configuration specified. Assuming default configuration...")
+		set(OGS_FEM ON)
+                set(counter 1)
+	endif(counter EQUAL 0)
 
-	IF(counter GREATER 1)
-		MESSAGE(FATAL_ERROR "Error: More than one OGS configuration given (${counter}). Please use only one of the following configurations:
+	if(counter GREATER 1)
+		message(FATAL_ERROR "Error: More than one OGS configuration given (${counter}). Please use only one of the following configurations:
 			OGS_FEM (Default FEM configuration)
 			OGS_FEM_SP
 			OGS_FEM_MPI
@@ -264,40 +264,40 @@ FUNCTION(CHECK_CONFIG)
 			OGS_FEM_PETSC
 			OGS_FEM_PETSC_GEMS
 			OGS_FEM_CAP")
-	ENDIF(counter GREATER 1)
+	endif(counter GREATER 1)
 
-ENDFUNCTION()
+endfunction()
 
 # Creates one ctest for each googletest found in source files passed as arguments
 # number two onwards. Argument one specifies the testrunner executable.
-MACRO(ADD_GOOGLE_TESTS executable)
-	FOREACH ( source ${ARGN} )
-		FILE(READ "${source}" contents)
-		STRING(REGEX MATCHALL "TEST_?F?\\(([A-Za-z_0-9 ,]+)\\)" found_tests ${contents})
-		FOREACH(hit ${found_tests})
-			STRING(REGEX REPLACE ".*\\(([A-Za-z_0-9]+)[, ]*([A-Za-z_0-9]+)\\).*" "\\1.\\2" test_name ${hit})
-			ADD_TEST(${test_name} ${executable}  --gtest_output=xml --gtest_filter=${test_name} ${MI3CTestingDir})
+macro(ADD_GOOGLE_TESTS executable)
+	foreach ( source ${ARGN} )
+		file(READ "${source}" contents)
+		string(REGEX MATCHALL "TEST_?F?\\(([A-Za-z_0-9 ,]+)\\)" found_tests ${contents})
+		foreach(hit ${found_tests})
+			string(REGEX REPLACE ".*\\(([A-Za-z_0-9]+)[, ]*([A-Za-z_0-9]+)\\).*" "\\1.\\2" test_name ${hit})
+			add_test(${test_name} ${executable}  --gtest_output=xml --gtest_filter=${test_name} ${MI3CTestingDir})
 			# message ("Adding test: ${test_name}")
-		ENDFOREACH(hit)
-	ENDFOREACH()
-ENDMACRO()
+		endforeach(hit)
+	endforeach()
+endmacro()
 
 # copies the model files to the build dir and adds them as targets so that
 # the build files are re-built when the source model files change
-MACRO ( UPDATE_MODEL_FILES dirOUT fileLIST )
-	GET_FILENAME_COMPONENT( _tdir ${CMAKE_CURRENT_SOURCE_DIR} NAME )
+macro ( UPDATE_MODEL_FILES dirOUT fileLIST )
+	get_filename_component( _tdir ${CMAKE_CURRENT_SOURCE_DIR} NAME )
 	#message (STATUS "Copying files to ${dirOUT} from ${fileLIST}.\n")
-	FOREACH ( _file1 ${${fileLIST}} )
-		SET( _file ${CMAKE_CURRENT_SOURCE_DIR}/${_file1} )
-		GET_FILENAME_COMPONENT( _fdest ${_file} NAME )
-		SET( dest ${dirOUT}/${_fdest} )
+	foreach ( _file1 ${${fileLIST}} )
+		set( _file ${CMAKE_CURRENT_SOURCE_DIR}/${_file1} )
+		get_filename_component( _fdest ${_file} NAME )
+		set( dest ${dirOUT}/${_fdest} )
 		#message( STATUS "Copying ${_file} to ${dest} \n" )
 
 		#message (STATUS "Adding targets ${_tdir}.${_fdest}\n").\n")
-		ADD_CUSTOM_TARGET( ${_tdir}.${_fdest}
+		add_custom_target( ${_tdir}.${_fdest}
 			${CMAKE_COMMAND} -E copy_if_different
 			${_file} ${dest})
 
-		ADD_DEPENDENCIES( testrunner ${_tdir}.${_fdest} )
-	ENDFOREACH(_file1 ${${fileLIST}})
-ENDMACRO ( UPDATE_MODEL_FILES dirOUT fileLIST)
+		add_dependencies( testrunner ${_tdir}.${_fdest} )
+	endforeach(_file1 ${${fileLIST}})
+endmacro ( UPDATE_MODEL_FILES dirOUT fileLIST)
