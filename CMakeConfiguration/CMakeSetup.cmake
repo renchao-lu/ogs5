f83385928ec1ff13f5c6367e45fc2b908aba7153
Christoph Lehmann <christoph.lehmann@ufz.de>
2015-07-08 06:43:34 +0000

    moved setting CMP0054 to CMakeSetup.cmake
    git-svn-id: https://svn.ufz.de/svn/ogs/trunk/sources@14202 3895a583-e809-0410-9bb6-aa9463026377

--- CMakeConfiguration/CMakeSetup.cmake
+++ CMakeConfiguration/CMakeSetup.cmake
@@ -20,6 +20,10 @@ IF (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} GREATER 2.7)
 	CMAKE_POLICY(SET CMP0014 OLD)
 ENDIF ()
 
+IF(CMAKE_VERSION VERSION_GREATER 3.1)
+	CMAKE_POLICY(SET CMP0054 NEW)
+ENDIF()
+
 # Provide a way for Visual Studio Express users to turn OFF the new FOLDER
 # organization feature. Default to ON for non-Express users. Express users must
 # explicitly turn off this option to build CMake in the Express IDE...
