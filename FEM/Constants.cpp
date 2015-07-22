12f84437838f2498b3ab9fdf3b53bc8e35da240c
Wenqing Wang <wenqing.wang@ufz.de>
2015-07-22 14:01:30 +0200

    Added more constants to Constant.h/cpp.

--- FEM/Constants.cpp
+++ FEM/Constants.cpp
@@ -9,7 +9,15 @@
 namespace constant_group
 {
 
+// Thermal constant
 double ThermalConstant :: _celsius_zero_in_kelvin = 273.15;
+
+// Fluid
+double FluidConstant::_comp_mol_mass_air = 28.96;
+double FluidConstant::_comp_mol_mass_water = 18.016;
+double FluidConstant::_comp_mol_mass_n2 = 28.014;
+double FluidConstant::_gas_constant = 8314.41;
+double FluidConstant::_specific_gas_constant = 461.5;
 
 }
 
--- FEM/Constants.h
+++ FEM/Constants.h
@@ -16,9 +16,53 @@ class ThermalConstant
         {
             return _celsius_zero_in_kelvin;
         }
+
     private:
         static double _celsius_zero_in_kelvin;
 };
 
+class FluidConstant
+{
+    public:
+        static double ComponentMolarMassAir()
+        {
+            return _comp_mol_mass_air;
+        }
+
+        static double ComponentMolarMassWater()
+        {
+            return _comp_mol_mass_water;
+        }
+
+        static double ComponentMolarMassWaterN2()
+        {
+            return _comp_mol_mass_n2;
+        }
+
+        static double GasConstant()
+        {
+            return _gas_constant;
+        }
+
+        static double SpecificGasConstant()
+        {
+            return _specific_gas_constant;
+        }
+
+    private:
+        static double _comp_mol_mass_air;
+        static double _comp_mol_mass_water;
+        static double _comp_mol_mass_n2;
+        static double _gas_constant;
+        static double _specific_gas_constant;
+};
+
 }
 #endif //OGS_CONSTANTS_H
+
+/*
+FluidConstant::ComponentMolarMassAir()
+FluidConstant::ComponentMolarMassWater()
+FluidConstant::ComponentMolarMassWaterN2()
+FluidConstant::GasConstant()
+*/
