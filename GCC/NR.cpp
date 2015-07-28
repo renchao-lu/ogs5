98a7b89e65cb299527768c2581b4c5cf3f561b61
Lars Bilke <lars.bilke@ufz.de>
2015-07-28 16:13:28 +0200

    Removed numerical recipes implementations.

--- GCC/NR.cpp
+++ GCC/NR.cpp
@@ -11,117 +11,23 @@ using namespace std;
 NR::NR(void){}
 NR::~NR(void){}
 
-
-inline double NR::SIGN(const double &a, const double &b)
-	{return (double)(b >= 0 ? (a >= 0 ? a : -a) : (a >= 0 ? -a : a));}
-
-
-
-
 double dfridr(double func(double), const double x/*, const double h, double &err*/)
 {
-	double h=1.0e-3, err;
-
-	const int ntab=10;
-	const double con=1.4, con2=(con*con);
-	const double big=numeric_limits<double>::max();
-	const double safe=2.0;
-	int i,j;
-	double errt,fac,hh,ans=0.0;
-	double a[ntab][ntab];
-	if (h == 0.0) throw("h must be nonzero in dfridr.");
-	hh=h;
-	a[0][0]=(func(x+hh)-func(x-hh))/(2.0*hh);
-	err=big;
-	for (i=1;i<ntab;i++) {
-		hh /= con;
-		a[0][i]=(func(x+hh)-func(x-hh))/(2.0*hh);
-		fac=con2;
-		for (j=1;j<=i;j++) {
-			a[j][i]=(a[j-1][i]*fac-a[j-1][i-1])/(fac-1.0);
-			fac=con2*fac;
-			if(abs(a[j][i]-a[j-1][i])>abs(a[j][i]-a[j-1][i-1]))
-				errt=abs(a[j][i]-a[j-1][i]);
-			else
-				errt=abs(a[j][i]-a[j-1][i-1]);
-			if (errt <= err) {
-				err=errt;
-				ans=a[j][i];
-			}
-		}
-		if (abs(a[i][i]-a[i-1][i-1]) >= safe*err) break;
-	}
-	return ans;
+	// Removed
+	std::cout << "Error: NR::dfridr not implemented!" << std::endl;
+	return numeric_limits<double>::min();
 }
 
 double NR::dfridrX(double func(double,double), const double x, const double y/*, const double h, double &err*/)
 {
-	double h=1.0e-3, err;
-
-	const int ntab=10;
-	const double con=1.4, con2=(con*con);
-	const double big=numeric_limits<double>::max();
-	const double safe=2.0;
-	int i,j;
-	double errt,fac,hh,ans=0.0;
-	double a[ntab][ntab];
-	if (h == 0.0) throw("h must be nonzero in dfridr.");
-	hh=h;
-	a[0][0]=(func(x+hh,y)-func(x-hh,y))/(2.0*hh);
-	err=big;
-	for (i=1;i<ntab;i++) {
-		hh /= con;
-		a[0][i]=(func(x+hh,y)-func(x-hh,y))/(2.0*hh);
-		fac=con2;
-		for (j=1;j<=i;j++) {
-			a[j][i]=(a[j-1][i]*fac-a[j-1][i-1])/(fac-1.0);
-			fac=con2*fac;
-			if(abs(a[j][i]-a[j-1][i])>abs(a[j][i]-a[j-1][i-1]))
-				errt=abs(a[j][i]-a[j-1][i]);
-			else
-				errt=abs(a[j][i]-a[j-1][i-1]);
-			if (errt <= err) {
-				err=errt;
-				ans=a[j][i];
-			}
-		}
-		if (abs(a[i][i]-a[i-1][i-1]) >= safe*err) break;
-	}
-	return ans;
+	// Removed
+	std::cout << "Error: NR::dfridrX not implemented!" << std::endl;
+	return numeric_limits<double>::min();
 }
 
 double NR::dfridrY(double func(double,double), const double x, const double y/*, const double h, double &err*/)
 {
-	double h=1.0e-3, err;
-
-	const int ntab=10;
-	const double con=1.4, con2=(con*con);
-	const double big=numeric_limits<double>::max();
-	const double safe=2.0;
-	int i,j;
-	double errt,fac,hh,ans=0.0;
-	double a[ntab][ntab];
-	if (h == 0.0) throw("h must be nonzero in dfridr.");
-	hh=h;
-	a[0][0]=(func(x,y+hh)-func(x,y-hh))/(2.0*hh);
-	err=big;
-	for (i=1;i<ntab;i++) {
-		hh /= con;
-		a[0][i]=(func(x,y+hh)-func(x,y-hh))/(2.0*hh);
-		fac=con2;
-		for (j=1;j<=i;j++) {
-			a[j][i]=(a[j-1][i]*fac-a[j-1][i-1])/(fac-1.0);
-			fac=con2*fac;
-			if(abs(a[j][i]-a[j-1][i])>abs(a[j][i]-a[j-1][i-1]))
-				errt=abs(a[j][i]-a[j-1][i]);
-			else
-				errt=abs(a[j][i]-a[j-1][i-1]);
-			if (errt <= err) {
-				err=errt;
-				ans=a[j][i];
-			}
-		}
-		if (abs(a[i][i]-a[i-1][i-1]) >= safe*err) break;
-	}
-	return ans;
+	// Removed
+	std::cout << "Error: NR::dfridrY not implemented!" << std::endl;
+	return numeric_limits<double>::min();
 }
--- GCC/NR.h
+++ GCC/NR.h
@@ -1,13 +1,10 @@
-class NR 
+class NR
 {
 private:
 
 public:
 	NR(void);
 	~NR(void);
-/* Methods */
-	static double zbrent(double func(double), const double x1, const double x2, const double tol);
-	static inline double SIGN(const double &a, const double &b);
 
 	static double dfridr (double func(double),        const double x);
 	static double dfridrX(double func(double,double), const double x, const double y);
