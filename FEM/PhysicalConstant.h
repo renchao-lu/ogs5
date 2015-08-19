23befed51ceeff613b00ea57ddeb273d885a131b
Wenqing Wang <wenqing.wang@ufz.de>
2015-08-19 10:59:14 +0200

    Removed namespace indentation in PhysicalConstant.

--- FEM/PhysicalConstant.h
+++ FEM/PhysicalConstant.h
@@ -24,73 +24,73 @@
  */
 namespace PhysicalConstant
 {
-    /// Celsius zero in Kelvin
-    const double CelsiusZeroInKelvin = 273.15;
+/// Celsius zero in Kelvin
+const double CelsiusZeroInKelvin = 273.15;
 
-    /**
-      Ideal gas constant in SI standard units (J mol^-1 K^-1)
+/**
+  Ideal gas constant in SI standard units (J mol^-1 K^-1)
 
-      Source:               http://physics.nist.gov/cgi-bin/cuu/Value?r
-      Date visited:         2015-04-17
-      Standard uncertainty: 0.000 0075 J mol^-1 K^-1
-    */
-    const double IdealGasConstant = 8.3144621;
+  Source:               http://physics.nist.gov/cgi-bin/cuu/Value?r
+  Date visited:         2015-04-17
+  Standard uncertainty: 0.000 0075 J mol^-1 K^-1
+*/
+const double IdealGasConstant = 8.3144621;
 
-    /**
-     * Molar masses of certain elements and chemical compounds
-     */
-    namespace MolarMass
-    {
-        /**
-         * Water
-         *
-         * Source:       http://www.ciaaw.org/pubs/TSAW2013_xls.xls
-         * Date visited: 2015-05-28
-         *
-         * According to the IUPAC report the molar mass of O is in the range [15.999 03, 15.999 77] g/mol
-         * and the molar mass of H is in the range [1.007 84, 1.008 11] g/mol
-         */
-        const double Water = 0.018016; ///< kg mol^-1
+/**
+ * Molar masses of certain elements and chemical compounds
+ */
+namespace MolarMass
+{
+/**
+ * Water
+ *
+ * Source:       http://www.ciaaw.org/pubs/TSAW2013_xls.xls
+ * Date visited: 2015-05-28
+ *
+ * According to the IUPAC report the molar mass of O is in the range [15.999 03, 15.999 77] g/mol
+ * and the molar mass of H is in the range [1.007 84, 1.008 11] g/mol
+ */
+const double Water = 0.018016; ///< kg mol^-1
 
-        /**
-         * N_2
-         *
-         * Source:       http://www.ciaaw.org/pubs/TSAW2013_xls.xls
-         * Date visited: 2015-05-28
-         *
-         * According to the IUPAC report the molar mass of N is in the range [14.006 43, 14.007 28] g/mol
-         */
-        const double N2    = 0.028013; ///< kg mol^-1
+/**
+ * N_2
+ *
+ * Source:       http://www.ciaaw.org/pubs/TSAW2013_xls.xls
+ * Date visited: 2015-05-28
+ *
+ * According to the IUPAC report the molar mass of N is in the range [14.006 43, 14.007 28] g/mol
+ */
+const double N2    = 0.028013; ///< kg mol^-1
 
-        /**
-         * O_2
-         *
-         * Source:       http://www.ciaaw.org/pubs/TSAW2013_xls.xls
-         * Date visited: 2015-05-28
-         *
-         * According to the IUPAC report the molar mass of O is in the range [15.999 03, 15.999 77] g/mol
-         */
-        const double O2    = 0.032;    ///< kg mol^-1
+/**
+ * O_2
+ *
+ * Source:       http://www.ciaaw.org/pubs/TSAW2013_xls.xls
+ * Date visited: 2015-05-28
+ *
+ * According to the IUPAC report the molar mass of O is in the range [15.999 03, 15.999 77] g/mol
+ */
+const double O2    = 0.032;    ///< kg mol^-1
 
-        /**
-         * Air
-         *
-         * Source:       http://www.engineeringtoolbox.com/molecular-mass-air-d_679.html
-         *
-         */
-        const double Air    = 0.02897;    ///< kg mol^-1
-    }
+/**
+ * Air
+ *
+ * Source:       http://www.engineeringtoolbox.com/molecular-mass-air-d_679.html
+ *
+ */
+const double Air    = 0.02897;    ///< kg mol^-1
+}
 
-    /**
-      Specific gas constant (J/(kg K)) defined by
-      \f[
-      R_v = \frac{R}{M}
-      \f]
-      with \$M\$, the momlar mass
-    */
-    namespace SpecificGasConstant
-    {
-        const double WaterVapour = 461.504; ///< IdealGasConstant / MolarMass::Water;
-    }
+/**
+  Specific gas constant (J/(kg K)) defined by
+  \f[
+  R_v = \frac{R}{M}
+  \f]
+  with \$M\$, the momlar mass
+*/
+namespace SpecificGasConstant
+{
+const double WaterVapour = IdealGasConstant / MolarMass::Water; //=461.504;
+}
 }
 #endif //OGS_CONSTANTS_H
